10d97fa15ebdf3b165625d843033314597f84f81
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2020-05-04 10:03:10 +0100

    my method working, amirs not

--- scripts/igbsm.py
+++ scripts/igbsm.py
@@ -3,14 +3,16 @@ import Bio
 import io
 
 
-cdss = {input}
-{params}_isb = isb.IntraSpecificBlastCommandLine(cdss)
-{params}_isb.execute()
-{params}_percents = {params}_isb.calculate_percents()
+cdss = snakemake.input[0]
+cds_prefix = snakemake.params[0]
+
+cds_prexix_isb = isb.IntraSpecificBlastCommandLine(cdss)
+cds_prexix_isb.execute()
+cds_prexix_percents = cds_prexix_isb.calculate_percents()
 
 from reprophylo import *
-%matplotlib inline
+import matplotlib.pyplot
 plt.switch_backend('agg')
-{params}_hist = plt.hist({params}_percents, bins=200)
-plt.plot({params}_hist[1][1:-1],{params}_hist[0][:-1], color='red', linewidth=2)
-plt.savefig('{output}', dpi=900, frameon=False)
+cds_prexix_hist = plt.hist(cds_prexix_percents, bins=200)
+plt.plot(cds_prexix_hist[1][1:-1],cds_prexix_hist[0][:-1], color='red', linewidth=2)
+plt.savefig(snakemake.output[0], dpi=900, frameon=False)
